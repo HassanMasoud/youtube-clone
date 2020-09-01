06d570125cee2602b2732fda07ea7891f1293c4d
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-01 22:51:06 +0300

    Create basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 
+import { Grid } from "@material-ui/core";
+
+import youtube from "./api/youtube";
+
 class App extends Component {
   render() {
-    return <div>Youtube Clone App</div>;
+    return (
+      <Grid container spacing={16} justify="center">
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              {/* Search Bar */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* Video */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* Video list */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
