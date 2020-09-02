45b8d384a6d47392d686316ade11f63f7cb24c09
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-02 21:04:35 +0300

    Define video item logic

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { Grid, Paper, Typography } from "@material-ui/core";
 
-const VideoItem = () => {
-  return <h1>Video Item</h1>;
+const VideoItem = ({ video }) => {
+  return (
+    <Grid item xs={12}>
+      <Paper style={{ display: "flex", alignItems: "center" }}>
+        <img
+          style={{ marginRight: "20px" }}
+          alt="thumbnail"
+          src={video.snippet.thumbnail.medium.url}
+        />
+        <Typography variant="subtitle1">
+          <b>{video.snippet.title}</b>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
 };
 
 export default VideoItem;
